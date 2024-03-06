f08c2ff7e8774e3a678cd4b5f42c38db4d6e3470
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-06 09:35:11 +0200

    d3DC

--- week2/day3/DailyChallenge/main.py
+++ week2/day3/DailyChallenge/main.py
@@ -1,15 +1,15 @@
 #challenge1
-word = input('word: ')
-letter_indexes = {}
+# word = input('word: ')
+# letter_indexes = {}
 
 
-for index, letter in enumerate(word):
-    if letter in letter_indexes:
-        letter_indexes[letter].append(index)
-    else:
-        letter_indexes[letter] = [index]
+# for index, letter in enumerate(word):
+#     if letter in letter_indexes:
+#         letter_indexes[letter].append(index)
+#     else:
+#         letter_indexes[letter] = [index]
 
-print(letter_indexes)
+# print(letter_indexes)
 
 #challenge2
 items_purchase = {
@@ -20,24 +20,39 @@ items_purchase = {
 }
 dict={}
 buy=[]
+
 wallet = "$300"
 wallet_money = float(wallet.replace("$", "")) 
+
 print(wallet_money)
+
 for item, price in items_purchase.items():
-    items_purchase[item] = price[1:]
+    items_purchase[item] = float(price[1:].replace(',', ''))
+    if int(price)<=wallet_money:
+        wallet_money-price
+        buy.append(item)
+    else:
+        continue
 print(items_purchase)
 
-price = items_purchase.values()
-total=float(sum(price))
-while total<=wallet_money:
-    buy.append(items_purchase)
-    break
-print(items_purchase)
 
-total=0
-for i in items_purchase.values():
-        total += i
+# price = items_purchase.values()
 
+# total=float(sum(price))
+# total=0
 
+# for each_price in items_purchase.items():
+#         total=float(sum(price))
+# print(total)
+
+# while True:
+#    for each_price in items_purchase.items():
+#         total+=float(sum(price))
+#         if total>wallet_money:
+#             break
+#         buy.append(item)  
+#         if total < 300:
+#             break        
+#         print(buy)
 
 
