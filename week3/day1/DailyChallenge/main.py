26278f6461bd474780ef2369279c728f11f72ea5
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-10 17:04:50 +0200

    dc

--- week3/day1/DailyChallenge/main.py
+++ week3/day1/DailyChallenge/main.py
@@ -13,7 +13,21 @@ class Farm:
         
     def get_info(self):
         print("E-I-E-I-0!")
+    
+    def get_animal_types(self):
+        animal_type=[]
+        animal_type= sorted(list(self.animals.keys()))
+        print(animal_type)
+        return animal_type
+    # def get_short_info(self):
+    #     animal_types = self.get_animal_types()
+    #     string = ' '.join(str(element) for element in self.get_animal_types)
+    def get_short_info(self):
+        animal_types = self.get_animal_types()
+        animal_types_string = ', '.join(animal_types)
+        print(f"McDonald’s farm has {animal_types_string}.")
         
+
 
 Mcdo = Farm('macdo')
 Mcdo.add_animal('cow', 5)
@@ -22,3 +36,5 @@ Mcdo.add_animal('sheep')
 Mcdo.add_animal('cat', 3)
 
 Mcdo.get_info()
+Mcdo.get_animal_types()
+Mcdo.get_short_info()
--- week3/day1/ExerciseXPNinja/main.py
+++ week3/day1/ExerciseXPNinja/main.py
@@ -1,8 +1,17 @@
-class phone:
-    def __init__(self,phone_number,call_history):
-        self.phone_number=phone_number
-        call_history=[]
+class Phone:
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+        self.call_history = []  
     def call(self,other_phone):
-        self.other_phone=other_phone
-        caller=input("caller: ")
-        call_history
+        caller=(f'{self.phone_number} called {other_phone}')
+        self.call_history.append(caller)
+        print(self.call_history)
+
+phone1 = Phone("1234567890")
+phone2 = Phone("0987654321")
+
+phone1.call(phone2)
+phone2.call(phone1)
+
+# phone1.show_call_history()
+# phone2.show_call_history()
