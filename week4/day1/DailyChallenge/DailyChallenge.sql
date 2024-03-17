8000a804e2bf982044aa1e7c41e6f2d6ea8f8e6e
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-17 15:07:20 +0200

    dc

---
+++ week4/day1/DailyChallenge/DailyChallenge.sql
@@ -0,0 +1,42 @@
+-- CREATE TABLE actor(
+--     actor_id SERIAL PRIMARY KEY,
+--     first_name varchar(50) NOT NULL,
+-- 	last_name varchar(100) NOT NULL,
+-- 	age DATE NOT NULL,
+-- 	number_oscars SMALLINT NOT NULL
+-- )
+
+-- INSERT INTO actor (first_name,last_name,age,number_oscars)
+-- VALUES('Matt','Damon','08/10/1970',5)
+
+-- INSERT INTO actor (first_name,last_name,age,number_oscars)
+-- VALUES
+-- ('Goerge','Clooney','05/3/1961',2),
+-- ('Gal','Gadot','04/30/1985',3),
+-- ('Brad','Pitt','12/18/1963',23);
+
+-- INSERT INTO actor (first_name,last_name,age,number_oscars)
+-- VALUES('Harrison','Ford','08/13/1942',0);
+-- INSERT INTO actor (first_name,last_name,age,number_oscars)
+-- VALUES('Anne','Hathaway','11/12/1982',1);
+
+-- INSERT INTO actor (first_name,last_name,age,number_oscars)
+-- VALUES
+-- ('Maryl','Streep','06/22/1949',3),
+-- ('Robert','Downey Jr.','04/4/1965',1),
+-- ('Marilyn','Monroe','6/1/1926',5);
+
+-- SELECT COUNT (*) FROM actor
+
+INSERT INTO actor (first_name,last_name,age,number_oscars)
+VALUES
+('Tomer','08/15/1985',0);
+-- ('Louis','de Funes','',1);
+
+-- not working
+SELECT * FROM actor
+
+
+
+
+
