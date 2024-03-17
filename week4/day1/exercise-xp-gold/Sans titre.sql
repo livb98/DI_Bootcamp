36e53c06e42869599b293252282c6d79a18c7d5e
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-17 15:26:16 +0200

    exXP+

---
+++ week4/day1/exercise-xp-gold/Sans titre.sql
@@ -0,0 +1,9 @@
+
+-- SELECT first_name,last_name,birth_date FROM students WHERE student_id<=4 ORDER BY last_name ASC;
+
+-- SELECT first_name,last_name,birth_date FROM students ORDER BY birth_date DESC LIMIT 1
+
+-- SELECT first_name,last_name,birth_date FROM students LIMIT 3 OFFSET 2
+
+-- SELECT first_name,last_name,birth_date FROM students
+
