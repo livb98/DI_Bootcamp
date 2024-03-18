ff9f54c611800d8b772fc4f4cda8df5b7ec3b7ea
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-18 15:44:09 +0200

    exercisexp

--- week4/day2/exerciseXP1.sql
+++ week4/day2/exerciseXP1.sql
@@ -1,7 +1,11 @@
+-- 1
 -- SELECT * FROM items ORDER BY item_price ASC
 
+-- 2
 -- SELECT * FROM items WHERE item_price>=80 ORDER BY item_price DESC
 
+-- 3
 -- SELECT CONCAT(first_name,' ', last_name) AS full_name FROM customers ORDER BY full_name ASC
 
-SELECT last_name FROM customers ORDER BY last_name DESC
+-- 4
+-- SELECT last_name FROM customers ORDER BY last_name DESC
