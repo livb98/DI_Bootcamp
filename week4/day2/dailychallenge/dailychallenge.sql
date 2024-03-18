5e61e0d73246d15fc81b0c270d7071e43347d70c
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-18 16:08:55 +0200

    dc

---
+++ week4/day2/dailychallenge/dailychallenge.sql
@@ -0,0 +1,44 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- )
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar')
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- )
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL)
+
+
+
+
+-- Q1:
+-- 0
+
+-- Q2:
+-- 2
+
+-- Q3:
+-- 0
+
+-- Q4:
+-- 2
+
+
+
+
+
+
+
+
+
+
+
