d21ecc4aa27e54712df8cd1ca51b5d68e4477eba
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-01 16:11:51 +0300

    1

---
+++ week6/day2/exerciseXP/script.js
@@ -0,0 +1,94 @@
+// exercise1
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+function colorChoice(){
+    for (x in colors){
+        console.log(`${(Number(x)+1)}# choice is ${colors[x]}`);
+    }
+}
+
+colorChoice()
+
+if (colors.includes("Violet")){
+    console.log("yeah");
+}
+else {
+    console.log("no");
+}
+
+// exercise2
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th","st","nd","rd"];
+
+
+function colorChoice2(colors){
+    let [a,b,c,d] = ordinal
+            return colors.map ((i,index) =>
+               index == 0 ? `${(index+1)}${b} choice is ${i}` :
+               index == 1 ? `${index+1}${c} choice is ${i}` : 
+               index == 2 ? `${index+1}${d} choice is ${i}` :
+               `${(index+1)}${a} choice is ${i}`
+               ).join('\n')
+            
+        }
+    
+
+console.log(colorChoice2(colors))
+
+//exercise3
+//------1------
+//['bread', 'carrot', 'potato', 'chicken', 'apple', 'orange']
+
+//------2------
+//['U','S','A']
+
+// exercise4
+const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+             { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+             { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+             { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+             { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+             { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+             { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+
+let welcomeStudent = []
+const arr = users.map((item)=>{
+   welcomeStudent.push(`Hello ${item.firstName}`);
+
+})
+console.log(welcomeStudent);
+
+let fullStak = users.filter(item =>{
+    return item.role = 'Full Stack Resident'
+})
+
+console.log(fullStak);
+
+// exercise5
+const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away']
+
+const newEpic = epic.reduce((accumulator, currentValue) => {
+    return accumulator + ' ' + currentValue;
+});
+
+
+console.log(newEpic);
+
+// exercise6
+const students = [{name: "Ray", course: "Computer Science", isPassed: true}, 
+               {name: "Liam", course: "Computer Science", isPassed: false}, 
+               {name: "Jenner", course: "Information Technology", isPassed: true}, 
+               {name: "Marco", course: "Robotics", isPassed: true}, 
+               {name: "Kimberly", course: "Artificial Intelligence", isPassed: false}, 
+               {name: "Jamie", course: "Big Data", isPassed: false}];
+
+
+let arr = []
+let studentPass = students.filter((item) =>{
+    if (item.isPassed == true){
+        arr.push(item)
+        console.log(`Good job ${item.name}, you passed the course in ${item.course}`);
+    }
+})
+console.log(arr);
+
