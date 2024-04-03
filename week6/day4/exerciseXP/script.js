3796a2c05ebdb9d46920bbf1208b62e2bb44ecc2
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-03 14:57:22 +0300

    xp

---
+++ week6/day4/exerciseXP/script.js
@@ -0,0 +1,33 @@
+// exercise3
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+
+let newMarioGame = JSON.stringify(marioGame)
+
+console.log(newMarioGame);
+
+console.log(marioGame);
+
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
