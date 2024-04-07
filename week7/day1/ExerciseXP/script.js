2877d79af0c4ed35f715a2ac9849e08c2088a6ab
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-07 10:46:01 +0300

    w

---
+++ week7/day1/ExerciseXP/script.js
@@ -0,0 +1,14 @@
+// exercise1
+// Create a function called compareToTen(num) that takes a number as an argument.
+// The function should return a Promise:
+// the promise resolves if the argument is less than or equal to 10
+// the promise rejects if argument is greater than 10.
+
+// exercise2
+// Create a promise that resolves itself in 4 seconds and returns a “success” string.
+
+// exercise3
+// Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
+// Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
+
+
