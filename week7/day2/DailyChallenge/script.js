66de7eff603b2a208681d2e5638c878aa6285e31
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-08 16:43:01 +0300

    dc

--- week7/day2/DailyChallenge/script.js
+++ week7/day2/DailyChallenge/script.js
@@ -1,6 +1,52 @@
-// Use this Giphy API Random documentation. Use this API Key : hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My
-// In the HTML file, add a form, containing an input and a button. This input is used to fetch gif depending on a specific category.
-// In the JS file, create a program to fetch one random gif depending on the search of the user (ie. If the search is “sun”, append on the page one gif with a category of “sun”).
-// The gif should be appended with a DELETE button next to it. Hint : to find the URL of the gif, look for the sub-object named “images” inside the data you receive from the API.
-// Allow the user to delete a specific gif by clicking the DELETE button.
-// Allow the user to remove all of the GIFs by clicking a DELETE ALL button.
+const form = document.getElementById("form");
+const allGif = document.getElementById("gif")
+const delAll = document.createElement('button')
+delAll.type = 'button'
+delAll.textContent = 'delete all'
+allGif.appendChild(delAll)
+
+
+async function fetchGif () {
+
+    const newUrl = new URL (`https://api.giphy.com/v1/gifs/random?`);
+    const apiKey = "hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+    let input = document.getElementById("input");
+    let category = input.value;
+    newUrl.searchParams.append('tag', category); 
+    newUrl.searchParams.append('rating', 'g');
+    newUrl.searchParams.append('api_key', apiKey);
+    const url = newUrl.href;
+    const options = {
+        method: 'GET',
+    };
+    try {
+        let res = await fetch(url, options);
+        let obj = await res.json();
+        const gif = document.createElement("img");
+        const gifURL = obj.data.images.original.url;
+        gif.src = gifURL;
+        const del = document.createElement('button')
+        del.type = 'button'
+        del.textContent = 'delete'
+        del.addEventListener('click', function (){
+            gif.remove()
+            del.remove()
+        })
+        delAll.addEventListener('click',function(){
+            gif.remove()
+            del.remove()
+        })
+        allGif.appendChild(gif)
+        allGif.appendChild(del);
+        console.log(allGif);
+
+    } catch (e) {
+        console.log(`error => ${e}`);
+    }
+
+}
+
+
+
+fetchGif()
+
