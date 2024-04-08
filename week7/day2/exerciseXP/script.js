0f1e733cecd4110a2b90feab38d468a35d16eb63
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-08 14:49:55 +0300

    sdv

---
+++ week7/day2/exerciseXP/script.js
@@ -0,0 +1,55 @@
+// exercise1
+function fetchGiphy(){
+    const url ='https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+    const options = {
+        method: 'GET',
+    }
+
+    fetch(url,options)
+    .then((res) => {
+        return res.json()
+    })
+    .then((res) =>{
+        console.log(res.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+}
+fetchGiphy()
+
+// exercise2
+async function GiphySun(){
+    const url ='https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+    const options = {
+        method: 'GET'
+    }
+    try {
+        let res = await fetch(url, options);
+        let obj = await res.json();
+        let data = await Object.values(obj)[0]
+        console.log(Object.values(data).slice(1,11));  
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+GiphySun()
+
+// exercise3
+const fetchSwapi = async (url) => {
+
+    try {
+        const response = await fetch(url)
+        let objectStarWars = await response.json();
+        console.log(objectStarWars.result);
+    } catch (e) {
+        console.log(`error is ${e}`);
+    }
+}
+fetchSwapi("https://www.swapi.tech/api/starships/9/")
+
+// exercise4
+// calling
+// resolved
