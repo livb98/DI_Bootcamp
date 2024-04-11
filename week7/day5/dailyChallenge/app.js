f63dfcd3c1e4811659ef82de01030af6e1aecba4
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-04-11 20:50:44 +0300

    efg

--- week7/day5/dailyChallenge/app.js
+++ week7/day5/dailyChallenge/app.js
@@ -1,19 +1,38 @@
-const { AsyncLocalStorage } = require('async_hooks');
-const express = require('express')
-const emoji = [
+// // const express = require('express')
+// // const app = express()
+
+let emojiSelect = document.getElementById('emoji')
+let pic = document.getElementById('pic')
+let submit = document.getElementById('submit')
+let form = document.getElementById('emojiForm')
+
+
+const emojiArr = [
     { emoji: '😀', name: 'Smile' },
     { emoji: '🐶', name: 'Dog' },
     { emoji: '🌮', name: 'Taco' },
-    // Add more emojis here if needed
   ];
 
 
-  function getEmoji(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  }
+
+
+const randomIndex = Math.floor(Math.random() * emojiArr.length);
+let i = emojiArr[randomIndex]
+let y = Object.values(i)
+let z = y[0]
+
+pic.textContent = z
+
+emojiArr.forEach(emoji => {
+    const optionDiv = document.createElement('option');
+    optionDiv.value = emoji.name
+    optionDiv.textContent = emoji.name
+    emojiSelect.appendChild(optionDiv)
+
+  });
+
+
+//   console.log(Object.values(y)[1]);
   
-  const selectedEmoji = getEmoji(emoji);
-  console.log(selectedEmoji);
 
 
