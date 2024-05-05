d7e96cb39c0cfb375876b25a517cc2ec434e7e71
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-05-05 16:09:07 +0300

    dfw

---
+++ week9/dailyChallenge/dailychallenge/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import DemoCarousel from './image';
+
+function App() {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1>demo</h1>
+      <DemoCarousel />
+    </div>
+    
+  );
+}
+
+
+export default App;
