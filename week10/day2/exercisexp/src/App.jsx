c1345d54030e7e730552d76db5bd0e79d4e58e4d
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-05-14 10:18:39 +0300

    gfew

--- week10/day2/exercisexp/src/App.jsx
+++ week10/day2/exercisexp/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useReducer, useRef } from "react";
 import Toggle from "../component/Toggle";
 import "./App.css";
 
-// exercise1
 export const initialState = {
   count: 0,
   tasks: [],
@@ -10,6 +9,7 @@ export const initialState = {
 
 const ADD_TASK = "add_task";
 const TOGGLE_TASK = "toggle_task";
+const REMOVE_TASK = "remove_task"
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -25,8 +25,11 @@ export const reducer = (state, action) => {
         return task;
       });
       return { ...state, tasks: updatedTasks };
-    case DEL_TASK:
-      const delTask = task.sp
+      case REMOVE_TASK:
+        return {
+          ...state,
+          tasks: state.tasks.filter((task, index) => index !== action.payload)
+        };
     default:
       return state;
   }
@@ -45,13 +48,16 @@ function App() {
     dispatch({ type: TOGGLE_TASK, payload: index });
   };
 
+  const removeTask = (index) => {
+    dispatch({ type: REMOVE_TASK, payload: index });
+  };
+
   return (
     <>
       <div>
         <h2>Tasks</h2>
         <input ref={inputRef} />
         <button onClick={() => addTask()}>Add Task</button>
-
         <div style={{ display: 'inline' }}>
           {state.tasks.map((item, i) => {
             return (
@@ -62,6 +68,9 @@ function App() {
                     toggle={item.toggle} 
                     handleToggleChange={() => handleToggleChange(i)} 
                   />
+                </div>
+                <div>
+                  <button onClick={() => removeTask(i)}>Remove</button>
                 </div>
               </div>
             );
