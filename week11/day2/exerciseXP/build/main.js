97dd71c464bc62b2b080116f6193c487d1ea8cc1
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-05-21 15:53:00 +0300

    hh

---
+++ week11/day2/exerciseXP/build/main.js
@@ -0,0 +1,21 @@
+"use strict";
+// exercise1
+// Create a class Person with the following properties:
+// In this exercise, a class Person is created with different access modifiers for its properties: private for firstName and lastName, public for age, and protected for address. The constructor initializes these properties, and the method getFullName returns the full name of the person.
+class Person {
+    constructor(firstName, lastName, age, adress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.adress = adress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.adress = adress;
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+let A = new Person('a', "A", 1, "aa");
+console.log(A.getFullName());
